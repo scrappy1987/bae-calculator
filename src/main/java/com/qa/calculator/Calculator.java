66e82f40b16595ff041f228cc79fbc91dca70672
scrappy1987 <john.gordon@qa.com>
2019-12-03 10:28:53 +0000

    added add feature

---
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.qa.calculator;
+
+public class Calculator {
+
+	public String add(int num1, int num2) {
+		int number = num1 + num2;
+		String result = "The result is " + number;
+		return result;
+	}
+
+}

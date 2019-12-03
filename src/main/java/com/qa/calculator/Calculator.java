879c8bfafc9d6e92e361e275c87bec70de39571b
scrappy1987 <john.gordon@qa.com>
2019-12-03 10:29:49 +0000

    refactored add command

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -4,8 +4,7 @@ public class Calculator {
 
 	public String add(int num1, int num2) {
 		int number = num1 + num2;
-		String result = "The result is " + number;
-		return result;
+		return "The result is " + number;
 	}
 
 }
